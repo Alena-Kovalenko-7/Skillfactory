467cb57113cb4d221c6b62c419bdd01f8ed5f41c
Alena_Kovalenko <kovalenkopsb@yandex.ru>
2025-03-03 00:55:13 +0300

    Доработана отправка сообщений от юзера и ответ от сервера в задании 3

--- Modul_10_API/Exercise_3/JS/chat.js
+++ Modul_10_API/Exercise_3/JS/chat.js
@@ -1,4 +1,5 @@
-const wsUrl = 'wss://echo-ws-service.herokuapp.com'
+// const wsUri = 'wss://echo-ws-service.herokuapp.com'
+const wsUri = 'wss://echo.websocket.org/';
 
 const btnSend = document.querySelector('.btn-send')
 const input = document.querySelector(".chat-massage");
@@ -6,14 +7,46 @@ const btnGeo = document.querySelector('.btn-geolocation')
 const chatConteiner = document.querySelector('.chat-container')
 
 let websocket;
+openWebSocket();
+  
+btnSend.addEventListener('click', () => {
+  const message = input.value;
+  if (message.length == 0) {
+    return;
+  }
+  writeToScreen(message);
+  input.value = "";
 
+  websocket.send(message);
 
-btnSend.addEventListener("click", () => {
-    if (input.value.length == 0) return;
-    const divMessageUser = document.createElement("div");
-    divMessageUser.classList.add("chat-container-user");
-    divMessageUser.textContent = input.value;
-    chatConteiner.appendChild(divMessageUser);
-    console.log(input.value);
-    input.value = "";
-  });
+  // websocket.close();
+  // websocket = null;
+});
+
+function openWebSocket()
+{
+  websocket = new WebSocket(wsUri);
+  websocket.onopen = function(evt) {
+    console.log("CONNECTED");
+  };
+  websocket.onclose = function(evt) {
+    console.log("DISCONNECTED");
+  };
+  websocket.onmessage = function(evt) {
+    writeToScreen(evt.data, "server");
+  };
+  websocket.onerror = function(evt) {
+    writeToScreen(
+      'ERROR: ' + evt.data
+    );
+  };
+}
+
+function writeToScreen(message, typeMessage = "user") {
+  const divMessage = document.createElement("div");
+  divMessage.textContent = message;
+  divMessage.classList.add("chat-container-" + typeMessage);
+  chatConteiner.appendChild(divMessage);
+  console.log(message);
+}
+  
